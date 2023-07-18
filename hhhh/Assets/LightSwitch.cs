f523f63ca38ae6a1cac8627ad9ef493b23244d5a
F1amezy <flamezybiz@gmail.com>
2023-07-18 14:40:51 -0500

    g

---
+++ hhhh/Assets/LightSwitch.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour
+{
+    public bool onSwitch;
+    public bool lightStatus;
+    public GameObject theLight;
+    void OnTriggerEnter(Collider other)
+    {
+        onSwitch = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        onSwitch = false;
+    }
+    void Update()
+    {
+        if (theLight.active == true)
+        {
+            lightStatus = true;
+        }
+        else
+        {
+            lightStatus = false;
+        }
+        if (onSwitch)
+        {
+            if (lightStatus)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    theLight.active = false;
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    theLight.active = true;
+                }
+            }
+        }
+    }
+    void OnGUI()
+    {
+        if (onSwitch)
+        {
+            if (lightStatus)
+            {
+                GUI.Box(new Rect(0, 0, 200, 20), "Press E to turn off the light");
+            }
+            else
+            {
+                GUI.Box(new Rect(0, 0, 200, 20), "Press E to turn on the light");
+            }
+        }
+    }
+}
