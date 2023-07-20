988dcd2f2f7135f1d9511939c551e67a7953ae84
F1amezy <flamezybiz@gmail.com>
2023-07-20 06:00:36 -0500

    h

--- hhhh/Assets/Scripts/Fullscreen.cs
+++ hhhh/Assets/Scripts/Fullscreen.cs
@@ -4,37 +4,9 @@ using UnityEngine;
 
 public class Fullscreen : MonoBehaviour
 {
-    private const string FullscreenKey = "IsFullscreen";
-
-    private bool isFullscreen;
-
-    private void Start()
-    {
-        // Load the fullscreen state from PlayerPrefs (0 for false, 1 for true)
-        isFullscreen = PlayerPrefs.GetInt(FullscreenKey, 1) == 1;
-
-        // Apply the fullscreen state
-        ApplyFullscreen();
-    }
-
     public void Change()
     {
-        // Toggle the fullscreen state
-        isFullscreen = !isFullscreen;
-
-        // Apply the fullscreen state
-        ApplyFullscreen();
-
-        // Save the fullscreen state to PlayerPrefs (0 for false, 1 for true)
-        PlayerPrefs.SetInt(FullscreenKey, isFullscreen ? 1 : 0);
-        PlayerPrefs.Save();
-
-        print("Changed screen mode");
-    }
-
-    private void ApplyFullscreen()
-    {
-        // Set the fullscreen state
-        Screen.fullScreen = isFullscreen;
+        Screen.fullScreen = !Screen.fullScreen;
+        print("changed screen mode");
     }
 }
